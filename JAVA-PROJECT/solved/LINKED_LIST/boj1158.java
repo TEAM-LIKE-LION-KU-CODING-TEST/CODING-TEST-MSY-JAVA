878345f2d9f1e8453cea69bcfc750f5de558c915
unknown <judemin2087@naver.com>
2024-09-05 13:56:59 +0900

    [SOLVED] : boj1158

---
+++ JAVA-PROJECT/solved/LINKED_LIST/boj1158.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class boj1158 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<Integer> list = new LinkedList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i = 0;i < N;i++)
+            list.add(i + 1);
+        
+        bw.write("<");
+        bw.flush();
+        // 만약 K가 전체 사람 수보다 클 경우 처리
+        int now = (K - 1) % list.size();
+        for(int i = 0;i < N - 1;i++){
+            bw.write(list.remove(now) + ", ");
+            bw.flush();
+            
+            // 사람이 제거되었으므로 1을 빼서 순서 유지
+            now += K - 1;
+            // now == list.size()일 경우
+            // 첫 번째 (현재 0번) 사람을 제거해야 한다
+            if(now >= list.size())
+                now %= list.size();
+        }
+        bw.write(list.get(0) + ">");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
--- JAVA-PROJECT/src/template.md
+++ JAVA-PROJECT/src/template.md
@@ -1,3 +1,6 @@
+### 바킹독의 실전 알고리즘
+https://github.com/encrypted-def/basic-algo-lecture/tree/master/workbook
+
 ```java
 public class Main {
     public static void main(String[] args) throws Exception {
