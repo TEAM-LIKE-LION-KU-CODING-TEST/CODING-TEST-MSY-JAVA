1d741d4c589daf3a1859f3293d5e1fc4f0c8b0fc
unknown <judemin2087@naver.com>
2024-09-05 12:15:34 +0900

    [UPDATE] UPDATED README

--- JAVA-PROJECT/src/App.java
+++ JAVA-PROJECT/src/App.java
@@ -1,5 +1,36 @@
+import java.io.*;
+
+// ASCII a : 97, z : 122
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        int [] arr1 = new int[27];
+        int ans = 0;
+        int i;
+
+        String st1 = br.readLine();
+        String st2 = br.readLine();
+
+        for(i = 0;i < st1.length();i++){
+            char now = st1.charAt(i);
+            arr1[now - 'a']++;
+        }
+
+        for(i = 0;i < st2.length();i++){
+            char now = st2.charAt(i);
+            arr1[now - 'a']--;
+        }
+
+        for(i = 0;i < 27;i++){
+            int now = arr1[i];
+            if(now < 0)
+                now *= -1;
+            
+            ans += now;
+        }
+
+        pw.println(ans);
+        pw.flush();
     }
 }
