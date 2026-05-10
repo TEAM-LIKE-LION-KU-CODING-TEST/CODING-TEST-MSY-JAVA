46b452d697d9b9205c8f74e9bb478bd87397ae0d
judemin <judemin2087@naver.com>
2026-05-10 18:56:26 +0900

    [SOLVED] 폰켓몬

---
+++ programmers/해시/폰켓몬.py
@@ -0,0 +1,11 @@
+def solution(nums):
+    answer = 0
+    
+    N = len(nums)
+    nums_set = set(nums)
+    if len(nums_set) > (N//2):
+        answer = N//2
+    else:
+        answer = len(nums_set)
+    
+    return answer
